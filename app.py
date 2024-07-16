fe62484aec533c7cd291038cf278f8e2e747b10f
MoonWalker404 <crossbeat71@gmail.com>
2024-07-16 21:00:54 +0300

    add all work

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/index')
 def home():
     return render_template('index.html')
 
@@ -15,4 +15,4 @@ def contacts():
     return render_template('contacts.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True)
